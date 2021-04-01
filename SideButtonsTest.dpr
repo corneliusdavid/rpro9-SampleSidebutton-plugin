687bfbdd65c56f106eced0173813a24c38ec63cb
David Cornelius <dev@corneliusconcepts.com>
2021-04-01 09:22:57 -0700

    removed the discover unit from being explicitly linked and now uses library paths; now compiled with Delphi 10.4

--- SideButtonsTest.dpr
+++ SideButtonsTest.dpr
@@ -3,7 +3,6 @@ library SideButtonsTest;
 uses
   ComServ,
   SideButtonsTest_TLB in 'SideButtonsTest_TLB.pas',
-  uPluginDiscover in 'V:\lib\RPro9\shared\Plugins\uPluginDiscover.pas',
   uPluginGlobals in 'uPluginGlobals.pas',
   uInventorySideButtonPlugin in 'uInventorySideButtonPlugin.pas',
   uCustomerSideButtonPlugin in 'uCustomerSideButtonPlugin.pas';
